241804398a53cfeae0de4aaa5d2484c8825711b1
Damilare <damilare182@gmail.com>
2020-07-14 23:31:41 +0100

    added function to determine squared error

--- regression.py
+++ regression.py
@@ -15,15 +15,28 @@ def get_y_intercept_and_m(x, y):
     b = mean(y) - m*(mean(x))
     return m, b
 
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regression = squared_error(ys_orig, ys_line)
+    squared_error_mean = squared_error(ys_orig, y_mean_line)
+
+    return 1-(squared_error_regression/squared_error_mean)
 
 slope, intercept = get_y_intercept_and_m(xs, ys)
 regression_line = [(slope*x)+intercept for x in xs]
 
-predict_x = 10.0
-predict_y = (slope*predict_x) + intercept
+r_squared = coefficient_of_determination(ys, regression_line)
 
-plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y)
+print(r_squared)
 
-plt.plot(xs, regression_line)
-plt.show()
+#predict_x = 10.0
+#predict_y = (slope*predict_x) + intercept
+
+#plt.scatter(xs, ys)
+#plt.scatter(predict_x, predict_y)
+
+#plt.plot(xs, regression_line)
+#plt.show()
